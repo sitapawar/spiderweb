7aa137c328d23ee52003d2f68c799b4605638549
sitapawar <sita_pawar@brown.edu>
2024-07-05 14:40:29 -0400

    working update

--- vis-graph/src/App.js
+++ vis-graph/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import GraphView from './components/graph';
 import Navbar from './components/navbar';
+import WorldCup from './components/worldcup';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <GraphView />
+      {/* <GraphView /> */}
+      <WorldCup />
     </div>
   );
 }
