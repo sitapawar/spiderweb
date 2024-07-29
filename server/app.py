537d6740b23d1927d23c07a513d645da0a79927a
sitapawar <sita_pawar@brown.edu>
2024-07-29 07:32:21 -0400

    app

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, json 
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,5 +10,16 @@ CORS(app)  # This will enable CORS for all routes
 def home():
     print('running')
     return "Hello, Flask!"
+
+from flask import request
+
+@app.route("/recieve_data", methods=['POST', 'GET'])
+def recieve_data():
+    if request.method == "POST":
+        data = request.get_data()
+        data = json.loads(data)
+        # Do Stuff Here
+        return data
+    
 if __name__ == '__main__':
     app.run(debug=True)
